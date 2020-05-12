6a45e0b75f36f45411bbc6917b017535a36cf58d
wangliang <236179666@qq.com>
2020-05-12 12:21:34 +0000

    注册视图表单验证

--- application/index/controller/Register.php
+++ application/index/controller/Register.php
@@ -35,10 +35,38 @@ class Register extends Base
      */
     public function save(Request $request,User $user)
     {
-        $user -> save($request -> post());
-        $message = '注册成功';
+        if(!$request -> isPost() || $request -> isAjax()){
+            return $this -> error('对不起，你访问页面不存在。');
+        }
+
+        try{
+            //保存表单提交数据
+            $user -> save($request -> post());
+        }catch(\Exception $e){
+            return $this -> error('对不起，注册失败。');
+        }
+
         //成功后跳转到注册表单页面
-        return $this -> success($message,'[page.root]');
+        return $this -> success('恭喜你注册成功！','[page.root]');
+    }
+
+    /**
+     * 验证字段值是否唯一
+     * @Author   zhanghong(Laifuzi)
+     */
+    public function check_unique(Request $request)
+    {
+        if(!$request -> isAjax()){
+            return $this -> redirect('[page.signup]');
+        }
+
+        $param = $request -> post();
+        $is_valid = User::checkFieldUnique($param);
+        if($is_valid){
+            echo("true");
+        }else{
+            echo("false");
+        }
     }
 
     /**
